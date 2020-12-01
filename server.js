61d7e78122c4f6f07836d6e1db927366ffd4b102
zemiguel20 <migli.coutinho@gmail.com>
2020-12-01 23:57:56 +0000

    express server added to serve built angular app, and template deleted

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('./dist/G5-FrontEnd'));
+
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/G5-FrontEnd' })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
