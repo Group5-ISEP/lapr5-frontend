c791a0dee96fe306c4a349f5f89b9d1d73995ca8
zemiguel20 <migli.coutinho@gmail.com>
2020-12-02 00:38:36 +0000

    #8 Represent map - map renderer component squeleton added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapRendererComponent } from './map-renderer/map-renderer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapRendererComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/map-renderer/map-renderer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-renderer',
+  templateUrl: './map-renderer.component.html',
+  styleUrls: ['./map-renderer.component.css']
+})
+export class MapRendererComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
