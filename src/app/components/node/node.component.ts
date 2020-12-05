48ac9b4ef885954eb38ed2775f57b3fdba83a003
patseho <juanv4004@gmail.com>
2020-12-05 18:07:20 +0000

    #2 Create node - SPA - frontend elements created Created HTML and CSS page as well as TS file and node service

---
+++ src/app/components/node/node.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NodeService } from '../../services/node.service';
+
+export interface INode {
+  shortName: string,
+  name: string,
+  isDepot: boolean,
+  isReliefPoint: boolean,
+  longitude: number,
+  latitude: number
+}
+
+@Component({
+  selector: 'app-node',
+  templateUrl: './node.component.html',
+  styleUrls: ['./node.component.css']
+})
+export class NodeComponent implements OnInit {
+
+  node = new FormGroup({
+    shortName: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    isDepot: new FormControl('', [Validators.required]),
+    isReliefPoint: new FormControl('', [Validators.required]),
+    longitude: new FormControl('', [Validators.required]),
+    latitude: new FormControl('', [Validators.required])
+  })
+
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    const body = {
+      shortName: this.node.value['shortName'],
+      name: this.node.value['name'],
+      isDepot: this.node.value['isDepot'],
+      isReliefPoint: this.node.value['isReliefPoint'],
+      longitude: this.node.value['longitude'],
+      latitude: this.node.value['latitude']
+    }
+    //console.log(body);
+    
+    this.nodeService.create(body)
+      .subscribe(
+        res => {
+
+        },
+        err => {
+          console.log(err);
+          alert("The node couldn't be created");
+        }
+      );
+  }
+
+}
---
+++ src/app/services/node.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { INode } from '../components/node/node.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NodeService {
+
+  constructor(private http: HttpClient) { }
+
+  create(node: INode) {
+    console.log("Creating node with:\n");
+    console.log(node);
+    return this.http.post('http://localhost:3000/api/node', node);
+  }
+}
