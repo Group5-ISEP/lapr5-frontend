2b07abb49b01b39434b68dd705ec6b349aa87ae0
zemiguel20 <migli.coutinho@gmail.com>
2020-12-05 22:58:34 +0000

    path service added with mock paths

---
+++ src/app/path.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import Path from './domain/Path';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PathService {
+
+  mockPaths: Path[] = [
+    {
+      lineCode: "201",
+      direction: "To",
+      firstNode: "EZC",
+      lastNode: "BOAV",
+      segmentList: [
+        { startNode: "EZC", endNode: "FTM", distance: 1319, duration: 20, order: 1 },
+        { startNode: "FTM", endNode: "BOAV", distance: 2850, duration: 15, order: 2 }
+      ],
+      isEmpty: false
+    }
+  ]
+
+  constructor() { }
+
+  //TODO: IMPLEMENT HTTP
+  getPaths(): Observable<Path[]> {
+    return of(this.mockPaths)
+  }
+}
